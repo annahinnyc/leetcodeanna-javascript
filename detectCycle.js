0d0ceafe24066af1824f4c680ccbcfd159374451
Annah Patterson <patterson.annah@gmail.com>
2023-03-25 00:37:00 -0400

    detectCycle

---
+++ detectCycle.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+      slow = slow.next;
+      fast = fast.next.next;
+
+      if (slow === fast) {
+          // Cycle detected
+          slow = head;
+
+          while (slow !== fast) {
+              slow = slow.next;
+              fast = fast.next;
+          }
+
+          return slow;
+      }
+  }
+
+  return null;
+};
